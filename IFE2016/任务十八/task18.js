bfaaee5a5504ab0eb4b4b821c607247933110c59
Nan Jiang <nj499521010@gmail.com>
2018-07-06 18:44:36 +0800

    完成任务十八

---
+++ IFE2016/任务十八/task18.js
@@ -0,0 +1,107 @@
+var task18  = {
+	taskArr: [10, 3, 20, 11],
+	number : /^\d+$/,
+	leftPush: function (num) {
+		this.taskArr.unshift(num);
+		renderArr();
+	},
+
+	rightPush: function (num) {
+		this.taskArr.push(num);
+		renderArr();
+	},
+
+	leftPop: function () {
+		if (this.taskArr.length === 0){
+			alert('队列为空');
+		} else {
+			this.taskArr.shift();
+			renderArr();
+		}
+	},
+
+	rightPop: function () {
+	if (this.taskArr.length === 0){
+		alert('队列为空');
+	} else {
+		this.taskArr.pop();
+		renderArr();
+	}
+}
+};
+
+/**
+ * addEventHandler方法
+ * 跨浏览器实现事件绑定
+ */
+function addEventHandler(ele, event, handler) {
+	if (ele.addEventListener){
+		ele.addEventListener(event, handler, false);
+	} else if (ele.attachEvent){
+		ele.attachEvent("on"+event, handler);
+	} else {
+		ele["on" + event] = handler;
+	}
+}
+
+/**
+ * renderArr 方法
+ * 渲染arr
+ */
+function renderArr(){
+	var arr = document.getElementsByClassName('arr')[0];
+	var arrLength = task18.taskArr.length;
+	arr.innerHTML = '';
+	if (arrLength <= 0){
+		arr.textContent = '队列为空, 请为队列添加内容';
+	} else {
+		for (var i = 0; i < task18.taskArr.length; i++) {
+			var span = document.createElement('span');
+			span.textContent = task18.taskArr[i];
+			arr.appendChild(span);
+		}
+	}
+}
+
+/**
+ * initBtn 方法
+ * 为按钮绑定事件
+ */
+function initBtns() {
+	var buttons = document.getElementsByTagName('button');
+	var inputs = document.getElementsByTagName('input');
+	addEventHandler(buttons[0], 'click', function() {
+		var input = inputs[0].value;
+		if (task18.number.test(input)){
+			console.log(task18.number.test('sd'));
+			task18.leftPush(input);
+		} else {
+			alert('请输入一个整数');
+		}
+	})
+
+	addEventHandler(buttons[1], 'click', function() {
+		var input = inputs[0].value;
+		if (task18.number.test(input)){
+			task18.rightPush(input);
+		} else {
+			alert('请输入一个整数');
+		}
+	})
+
+	addEventHandler(buttons[2],'click', function() {
+		task18.leftPop()
+	})
+
+
+	addEventHandler(buttons[3],'click', function() {
+		task18.rightPop()
+	})
+}
+
+function init(){
+	initBtns();
+	renderArr();
+}
+
+init();
